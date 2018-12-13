4c8b754516c00292436791b36a3bbd7f114590cc
松井　祐希 <yuhki@GloogaDualiesDeco.local>
2018-12-13 11:59:24 +0900

    クラスを分割

--- main.py
+++ main.py
@@ -14,46 +14,33 @@ class Par: # 入力値や各種パラメータを扱うクラス
   def input_eva(self):
     self.A1,self.A2,self.A3,self.B1,self.B2,self.B3 = map(float,input().split()[1:])
 
-
-  # MACHINE情報の入力受付
-  # [マシン番号, 製造時間係数, 段取り時間係数]でまとめておく
-  def input_machine(self):
-    self.MACHINE = np.empty((0,3),int)
-    c = list(map(int,input().split()[1:]))
-    d = list(map(int,input().split()[1:]))
-    for i in range(self.M):
-      self.MACHINE = np.append(self.MACHINE, np.array([[i+1,c[i],d[i]]]), axis=0)
-
-  # BOMの入力受付
-  def input_bom(self):
-    self.BOM = np.empty((0,4),int)
-    for i in range(self.BL):
-      self.BOM = np.append(self.BOM,np.array([list(map(int,input().split()[1:]))]),axis=0)
-      
-  # ORDERの入力受付
-  def input_order(self):
-    self.ORDER = np.empty((0,5),int)
-    for i in range(self.R):
-      self.ORDER = np.append(self.ORDER,np.array([list(map(int,input().split()[1:]))]),axis=0)
-
-  # 傾向の決定
-  def trend(self):
-    if(self.A2 == max(self.A1,self.A2,self.A3)):
-      self.trend = 2
-    elif(self.A1 == max(self.A1,self.A3)):
-     self.trend = 1
-    else:
-      self.trend = 3
-
-
-  # 全ての初期入力を受け付ける
   def __init__(self):
     self.input_header()
     self.input_eva()
-    self.input_machine()
-    self.input_bom()
-    self.input_order()
-    self.trend()
+
+
+class Machine(object): # マシン情報を扱うクラス
+  def __init__(self,num,c,d):
+    self.num = num
+    self.c = c
+    self.d = d
+
+
+class Bom(object): # BOM情報を扱うクラス
+  def __init__(self,i,p,m,t):
+    self.i = i
+    self.p = p
+    self.m = m
+    self.t = t
+      
+  # ORDERの入力受付
+class Order(object): # ORDER情報を扱うクラス
+  def __init__(self,r,i,e,d,p):
+    self.r = r
+    self.i = i
+    self.e = e
+    self.d = d
+    self.p = p
 
 #役にたつかもしれない
 #def aa(self):
@@ -61,8 +48,23 @@ class Par: # 入力値や各種パラメータを扱うクラス
 
 def main(): #メイン関数
   par = Par() # 入力受付
-  print(par.trend)
 
+  
+  machine = [[] for i in range(par.M)]
+  c = list(map(int,input().split()[1:]))
+  d = list(map(int,input().split()[1:]))
+  for i in range(par.M):
+    machine[i] = Machine(i+1,c[i],d[i])
+
+  bom = [[] for i in range(par.BL)]
+  for j in range(par.BL):
+    i,p,m,t = map(int,input().split()[1:])
+    bom[j] = Bom(i,p,m,t)
+
+  order = [[] for i in range(par.R)]
+  for j in range(par.R):
+    r,i,e,d,p = list(map(int,input().split()[1:]))
+    order[j] = Order(r,i,e,d,p)
 
 if __name__ == "__main__":
   main()
