28bc45927cbc1a2d0c1e69566ad4810d1a3660e1
松井　祐希 <yuhki@GloogaDualiesDeco.local>
2018-12-13 10:32:24 +0900

    trendの計算

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-#input
-#HEADER M I P R BL
-#EVALUATIONFACTOR A A A B B B
-#PRODUCTIONFACTOR C C … C
-#SETUPFACTOR D D … D
-#BOM i p m t
-#:
-#ORDER r i e d q
-
 import copy
 import numpy as np
-#main
-class Par: #変数を扱ったりするクラス
-  M = -1 #設備数(1..20,整数)
-  I = -1 #品目数(1..20,整数)
-  P = -1 #最大工程数(1..5,整数)
-  R = -1 #オーダ数(1..200,整数)
-  BL = -1 #BOM行数(1,2..,整数)
-  A1 = -1.0 #段取り時間ペナルティ係数(0≤A1≤100,実数)
-  A2 = -1.0 #納期遅れペナルティ係数(0≤A2≤100,実数)
-  A3 = -1.0 #着手遅延ポイント係数(0≤A3≤100,実数)
-  B1 = -1.0 #段取り時間べき乗数(0<B1≤2,実数)
-  B2 = -1.0 #納期遅れべき乗数(0<B2≤2,実数)
-  B3 = -1.0 #着手遅延べき乗数(0<B3≤2,実数)
 
+class Par: # 入力値や各種パラメータを扱うクラス
+  # 入力値以外のパラメータ
+  trend = -1 # スケジューリングの時の指向
+
+
+  # ヘッダパラメータの入力受付
   def input_header(self):
     self.M,self.I,self.P,self.R,self.BL = map(int,input().split()[1:])
 
+  # 評価用パラメータの入力受付
   def input_eva(self):
     self.A1,self.A2,self.A3,self.B1,self.B2,self.B3 = map(float,input().split()[1:])
 
+
+  # MACHINE情報の入力受付
+  # [マシン番号, 製造時間係数, 段取り時間係数]でまとめておく
   def input_machine(self):
     self.MACHINE = np.empty((0,3),int)
     c = list(map(int,input().split()[1:]))
@@ -36,27 +24,45 @@ class Par: #変数を扱ったりするクラス
     for i in range(self.M):
       self.MACHINE = np.append(self.MACHINE, np.array([[i+1,c[i],d[i]]]), axis=0)
 
+  # BOMの入力受付
   def input_bom(self):
     self.BOM = np.empty((0,4),int)
     for i in range(self.BL):
       self.BOM = np.append(self.BOM,np.array([list(map(int,input().split()[1:]))]),axis=0)
       
+  # ORDERの入力受付
   def input_order(self):
     self.ORDER = np.empty((0,5),int)
     for i in range(self.R):
       self.ORDER = np.append(self.ORDER,np.array([list(map(int,input().split()[1:]))]),axis=0)
 
+  # 傾向の決定
+  def trend(self):
+    if(self.A2 == max(self.A1,self.A2,self.A3)):
+      self.trend = 2
+    elif(self.A1 == max(self.A1,self.A3)):
+     self.trend = 1
+    else:
+      self.trend = 3
+
+
+  # 全ての初期入力を受け付ける
   def __init__(self):
     self.input_header()
     self.input_eva()
     self.input_machine()
     self.input_bom()
     self.input_order()
+    self.trend()
+
+#役にたつかもしれない
+#def aa(self):
+#  print(self.M)
+
+def main(): #メイン関数
+  par = Par() # 入力受付
+  print(par.trend)
 
 
-p = Par()
-
-print(p.M)
-print(p.MACHINE)
-print(p.BOM)
-print(p.BOM[2,2:])
+if __name__ == "__main__":
+  main()
