f970aeb9b29af53d070d7718720849e145b8c7dd
松井　祐希 <yuhki@GloogaDualiesDeco.local>
2018-12-29 11:14:43 +0900

    generator完成

--- genertor.py
+++ genertor.py
@@ -1,7 +1,7 @@
 import random
 
 print("input number(1 or 2 or 3)")
-n = input()
+n = int(input())
 
 eva = ""
 pro = ""
@@ -18,15 +18,27 @@ b3 = random.uniform(0.1,min(b1,b2))
 
 eva = "{}\t{}\t{}\t{}\t{}\t{}".format(a1,a2,a3,b1,b2,b3)
 
-
+print(n)
 if(n == 1):
-  for i in range(4):
-    pro += str(random.randint(1,))
+  k = 4
+
+elif(n == 2):
+  k = 10
+
+elif(n == 3):
+  k = 20 
 
 else:
-  print("ERROR")
+  k = 0
+print(k)
 
+for i in range(k):
+  pro += str(random.randint(1,10000))
+  setup += str(random.randint(1,10000))
+  if(i != -1):
+    pro += "\t"
+    setup += "\t"
 
 print("EVALUATIONFACTOR\t{}".format(eva))
-print("PRODUCTIONFACTOR")
-print("SETUPFACTOR")
+print("PRODUCTIONFACTOR\t{}".format(pro))
+print("SETUPFACTOR\t{}".format(setup))
